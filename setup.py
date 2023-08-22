7b64ef6eba1bf341e4cb90210c44915d9ede6ff2
dodda sai sachin chowdary <115567565+doddasaisachin@users.noreply.github.com>
2023-08-22 21:19:56 +0530

    Updated

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    author="iNeuron",
-    author_email="cloud@ineuron.ai",
+    author="Dodda Sai Sachin",
+    author_email="doddasaisachin@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
